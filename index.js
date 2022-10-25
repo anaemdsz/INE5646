35ed146b438f12e35d1a0a82b48117ec8312b5f9
anaemdsz <anaemerimdsz@gmail.com>
2022-10-25 06:19:47 -0300

    moved index and added page links

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const home = () => {
-    // TO-DO: Back to home page
+const logoClickHandler = () => {
+    // TO-DO: 
+    //     - If user is logged in go to home dashboard
+    //     - Else go to login screen
+    document.location.href = "http://127.0.0.1:5500/html/index.html"
 }
 
 const showSignup = () => {
@@ -9,6 +12,7 @@ const showSignup = () => {
 };
 
 const login = () => {
+    document.location.href = "http://127.0.0.1:5500/html/home.html"
     // TO-DO: 
     //     - Check if user is registered
     //     - Check if password is right
@@ -21,7 +25,6 @@ const signup = () => {
     //     - Check if passwords match (and if they are hard enough)
     //     - Add user+password to DB
 };
-
 
 
 
